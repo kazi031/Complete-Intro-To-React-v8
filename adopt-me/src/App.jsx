4cc4ca224736e3d0171c3efd2c45fa940a97219e
kazi031 <kazirafidraiyan031@gmail.com>
2023-10-05 16:03:28 +0600

    react-query

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -2,8 +2,18 @@
 import { createRoot } from "react-dom/client";
 // import Pet from "./Pet";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, //1000*60*10=10mins
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   // return React.createElement("div", {}, [
@@ -26,13 +36,15 @@ const App = () => {
   // ]);
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
