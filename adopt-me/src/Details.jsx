1cc3818a029012c9ce2f820673e10c04ae3f9729
kazi031 <kazirafidraiyan031@gmail.com>
2023-10-08 16:05:22 +0600

    Class Components

--- adopt-me/src/Details.jsx
+++ adopt-me/src/Details.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 // useParams Cant be used without a BrowserRouter
 import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
-  results.refetch();
+  //results.refetch();
 
   if (results.isLoading) {
     return (
@@ -20,6 +21,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
